8570f90e559562b56919c59f3661c93aa271dffe
Weeksly1312 <S.elhallaoui01@gmail.com>
2023-10-08 12:08:21 +0100

    condition in jsx

--- MyReactApp/src/assets/js/content.js
+++ MyReactApp/src/assets/js/content.js
@@ -1,4 +1,10 @@
 export const showAlert = () => {
-    alert('You clicked this button!');
-  };
-  
+  alert('You clicked this button!');
+};
+
+
+
+
+export const checkAge = (age) => {
+  return age === 20 ? 'Welcome!' : 'You are not 20 years old.';
+};
--- MyReactApp/src/components/Content.jsx
+++ MyReactApp/src/components/Content.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import "../assets/styles/body.css";
-import { showAlert } from "../assets/js/content";
+import { showAlert, checkAge } from "../assets/js/content";
+
+
 
 export const Content = () => {
+  const age = 20; // Set the age here
+  const message = checkAge(age);
   return (
     <>
       <button onClick={showAlert}>Click me</button>
+      <button>{message}</button>
     </>
   );
 };
-
-
-
-
