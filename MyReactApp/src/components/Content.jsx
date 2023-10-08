7a2263959ffcd3a97ef6063380c22dfe830638c9
Weeksly1312 <S.elhallaoui01@gmail.com>
2023-10-08 14:13:32 +0100

    add KEY

--- MyReactApp/src/components/Content.jsx
+++ MyReactApp/src/components/Content.jsx
@@ -6,11 +6,28 @@ export const Content = () => {
   const [age, setAge] = useState(20);
   const [message, setMessage] = useState(checkAge(age));
 
+  const people = ["Rowe", "Prevost", "Gare"];
+  const peopleList = people.map((person, i) => (
+    // expression goes here:
+    // <li>{`${i}  ${person}`}</li>
+    // key
+    <li key={'person_' + i}>{person}</li>
+  ));
+
   return (
     <>
       <button onClick={showAlert}>Click me</button>
+      <br/>
+      <br/>
       <button onClick={() => showMessage(setMessage)}>Show Message</button>
+      <br/>
+      <br/>
       <div className="test">{message}</div>
+      <br/>
+      <br/>
+      <div>
+        <ul>{peopleList}</ul>
+      </div>
     </>
   );
 };
